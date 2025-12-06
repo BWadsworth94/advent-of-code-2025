261e18f19ce3c3c1ad176abc16a35c95e331248f
Ben Wadsworth <wadsworth.benjamin.m@gmail.com>
2025-12-06 18:11:34 -0500

    small cleanup

--- src/aoc_2025/solutions/day_two.clj
+++ src/aoc_2025/solutions/day_two.clj
@@ -67,18 +67,6 @@
      0
      ranges)))
 
-;; 123123123
-
 (comment
   (part-one (read-input-file "two"))
-  (part-two (read-input-file "two"))
-  
-
- (apply + (flatten (part-two "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"))))
-  
-
-(part-two "1-10")
-
-(filter
- repeated-pattern?
- (range 824824821 824824827))
+  (part-two (read-input-file "two")))
