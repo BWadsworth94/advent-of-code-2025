1c9d426aff52aac4c7c77f66a59bc7501227fc9b
Ben Wadsworth <wadsworth.benjamin.m@gmail.com>
2025-12-06 12:22:01 -0500

    Refactor day-one part-one

--- src/aoc_2025/solutions/day_one.clj
+++ src/aoc_2025/solutions/day_one.clj
@@ -3,27 +3,42 @@
    [aoc-2025.util :refer [read-input-file]]
    [clojure.string :as str]))
 
+(defn- parse-instructions
+  "Produce a seq of [dir degree]; 'L32' => [['L', 32]]"
+  [input]
+  (->> input str/split-lines
+       (map
+        (comp (fn [[_ dir degree]]
+                [dir (parse-long degree)])
+              #(re-matches #"([L|R])(\d+)" %)))))
+
+(defn- rotate-dial
+  "'Rotates' the dial by the current instructions direction and degree.
+   (52 [L 3]) => 49; does not wrap at 0 or 100."
+  [dial instruction]
+  (let [[dir degree] instruction]
+    (if (= "L" dir)
+      (- dial degree)
+      (+ dial degree))))
+
+(defn- zero-point?
+  "Not handling wrapping of the dial means we can just check for a point to zero
+   by modding the current dial position by 100."
+  [dial]
+  (zero? (mod dial 100)))
+
 (defn part-one
   [input]
-  (let [instructions (->> input
-                          str/split-lines
-                          (map
-                           (comp
-                            (fn [[_ move degree]]
-                              [move (parse-long degree)])
-                            #(re-matches #"([L|R])(\d+)" %))))
-        {:keys [landings]} (reduce
-                            (fn [{:keys [current-position] :as acc} [move degree]]
-                              (let [new-position (if (= move "R")
-                                                   (+ current-position degree)
-                                                   (- current-position degree))]
-                                (-> acc
-                                    (update :landings conj new-position)
-                                    (assoc :current-position new-position))))
-                            {:landings []
-                             :current-position 50}
-                            instructions)]
-    (->> landings (filter #(zero? (mod % 100))) count)))
+  (let [instructions (parse-instructions input)]
+    (loop [zero-points-cnt 0
+           dial 50
+           instructions instructions]
+      (if-not (first instructions)
+        zero-points-cnt
+        (let [new-dial (rotate-dial dial (first instructions))]
+          (recur (if (zero-point? dial) (inc zero-points-cnt) zero-points-cnt)
+                 new-dial
+                 (rest instructions)))))))
 
 (comment
   (part-one (read-input-file "one")))
