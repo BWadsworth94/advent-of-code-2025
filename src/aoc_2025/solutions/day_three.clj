0b7aeb6bd0b7a6c134e19326b99ea120fd53b3b7
Ben Wadsworth <wadsworth.benjamin.m@gmail.com>
2025-12-09 21:30:17 -0500

    day 3 part 1

---
+++ src/aoc_2025/solutions/day_three.clj
@@ -0,0 +1,39 @@
+(ns aoc-2025.solutions.day-three 
+  (:require
+   [clojure.string :as str]
+   [aoc-2025.util :refer [read-input-file]]))
+
+(defn- get-max-tuple
+  [battery-row]
+  (let [digits (map parse-long (str/split battery-row #""))
+        sorted-digits (->> digits ((partial sort >)))
+        xs-past-max-digit (loop [digits digits]
+                            (if (= (first sorted-digits) (first digits))
+                              (rest digits)
+                              (recur (rest digits))))]
+    ;; if there are no elements after the max number, it's at the end of the list
+    ;; and the second max digit would be before it
+    (if-not (seq xs-past-max-digit)
+      (reverse (take 2 sorted-digits))
+      [(first sorted-digits) (apply max xs-past-max-digit)])))
+
+(defn- combine-ints
+  [[x y]]
+  (parse-long (format "%d%d" x y)))
+
+(defn part-one
+  [input]
+  (let [battery-rows (str/split-lines input)]
+    (apply +
+           (map
+            (comp combine-ints get-max-tuple)
+            battery-rows))))
+
+(comment
+  (part-one (read-input-file "three"))
+  )
+
+;; 818181911112111
+;;   find max value of digits
+;;   subs past that point
+;;   find max of rest
